0f9ace09f7c3e3e1e0d19b5f1b19094838c82b97
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-12-26 22:48:14 +0900

    test: Application 통합테스트 - student ~ 수강신청까지

--- Application/src/test/java/com/example/Application/service/IntegrationTest.java
+++ Application/src/test/java/com/example/Application/service/IntegrationTest.java
@@ -7,19 +7,17 @@ import com.example.Application.domain.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
-public class EnrollmentServiceTest {
+public class IntegrationTest {
 
     @Autowired
     private StudentService studentService;
@@ -33,7 +31,9 @@ public class EnrollmentServiceTest {
     @Autowired
     private EnrollmentService enrollmentService;
 
-    private Long createTestCase() {
+    @Test
+    void IntegrationSuccessTest() {
+        //given
         Student student = new Student();
         student.setName("abc123");
         student.setEmail("abc@google.com");
@@ -53,39 +53,36 @@ public class EnrollmentServiceTest {
         enrollment.setCourse(course);
         enrollment.setStudent(student);
 
-        studentService.join(student);
-        professorService.join(professor);
-        courseService.register(course);
-        return enrollmentService.apply(enrollment);
-    }
+        //when
+        Long studentId = studentService.join(student);
+        Optional<Student> findStudent = studentService.findOne(studentId);
 
-    @Test
-    void applySuccessTest() {
-        Long enrollmentId = createTestCase();
+        Long professorId = professorService.join(professor);
+        Optional<Professor> findProfessor = professorService.findOne(professorId);
+
+        Long courseId = courseService.register(course);
+        Optional<Course> findCourse = courseService.findOne(courseId);
+
+        Long enrollmentId = enrollmentService.apply(enrollment);
         Optional<Enrollment> findEnrollment = enrollmentService.findOne(enrollmentId);
 
+        //then
+        findStudent.ifPresent(student1 -> {
+            assertThat(student1.getName()).isEqualTo(student.getName());
+        });
+
+        findProfessor.ifPresent(professor1 -> {
+            assertThat(professor1.getName()).isEqualTo(professor.getName());
+        });
+
+        findCourse.ifPresent(course1 -> {
+            assertThat(course1.getTitle()).isEqualTo(course.getTitle());
+        });
+
         findEnrollment.ifPresent(enrollment1 -> {
-            assertThat(enrollment1.getEnrollmentId()).isEqualTo(enrollmentId);
+            assertThat(enrollment1.getStudent().getName()).isEqualTo(enrollment.getStudent().getName());
+            assertThat(enrollment1.getCourse().getTitle()).isEqualTo(enrollment.getCourse().getTitle());
         });
     }
 
-    @Test
-    void applyErrorTest() {
-        Long enrollmentId = createTestCase();
-
-        Optional<Enrollment> enrollment = enrollmentService.findOne(enrollmentId);
-
-        enrollment.ifPresent(enrollment1 -> {
-            assertThatThrownBy(() -> {
-                Student student = enrollment1.getStudent();
-                Course course = enrollment1.getCourse();
-
-                Enrollment newEnrollment = new Enrollment();
-                newEnrollment.setStudent(student);
-                newEnrollment.setCourse(course);
-
-                enrollmentService.apply(newEnrollment);
-            }).isInstanceOf(DataIntegrityViolationException.class);
-        });
-    }
 }
